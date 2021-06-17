61a0ed5de34445ad161db88c1dfa61bedf721a7e
mizaguirre <75349666+michaelizaguirre501@users.noreply.github.com>
2021-06-17 22:47:16 +0000

    mongo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const path = require("path");
+const connectDB = require("./config/database");
 
 const mainRoutes = require("./routes/main");
+
+
+//Connect To Database
+connectDB();
 
 //Using EJS for views
 app.set("views", path.join(__dirname, "views"));
@@ -14,6 +21,18 @@ app.use(express.static("public"));
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
